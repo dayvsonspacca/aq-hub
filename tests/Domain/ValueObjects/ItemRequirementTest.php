be10b0987a7ea222e39bba89f4d961c553401429
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-03-21 09:14:56 -0300

    tests: adds tests to ItemRequirement

---
+++ tests/Domain/ValueObjects/ItemRequirementTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\ValueObjects;
+
+use AqWiki\Domain\{ValueObjects, Entities, Enums};
+use PHPUnit\Framework\Attributes\Test;
+use AqWiki\Tests\TestCase;
+
+final class ItemRequirementTest extends TestCase
+{
+    public const ITEM_NAME        = 'Abyssal Angel Naval Commander';
+    public const ITEM_DESCRIPTION = 'Abyssal Angel Commanders sail the Celestial Seas, and are feared across the realms for the cutthroat way they have of taking their opponents down.';
+
+    private Entities\AqwItem $item;
+
+    protected function setUp(): void
+    {
+        $this->item = new class (
+            self::ITEM_NAME,
+            Enums\ItemRarity::RareRarity,
+            new ValueObjects\GameCurrency(0, Enums\CurrencyType::AdventureCoins),
+            new ValueObjects\GameCurrency(0, Enums\CurrencyType::AdventureCoins),
+            self::ITEM_DESCRIPTION
+        ) extends Entities\AqwItem {};
+    }
+
+    #[Test]
+    public function should_fail_when_player_dont_have_item()
+    {
+        $itemRequirement = new ValueObjects\ItemRequirement($this->item);
+
+        $this->assertSame(false, $itemRequirement->pass(
+            new Entities\Player(
+                80,
+                []
+            )
+        ));
+    }
+
+
+    #[Test]
+    public function should_pass_when_player_have_item()
+    {
+        $itemRequirement = new ValueObjects\ItemRequirement($this->item);
+
+        $this->assertSame(true, $itemRequirement->pass(
+            new Entities\Player(
+                80,
+                [$this->item]
+            )
+        ));
+    }
+}
