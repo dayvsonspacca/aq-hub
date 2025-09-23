706aa917cb039b8804a940a932ce2fb8c3e20327
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-09-23 13:39:51 -0300

    feat: enhance WeaponType capabilities

--- src/Items/Domain/Enums/WeaponType.php
+++ src/Items/Domain/Enums/WeaponType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace AqWiki\Items\Domain\Enums;
 
+use AqWiki\Shared\Domain\ValueObjects\Result;
+
 enum WeaponType
 {
     case Axe;
@@ -17,10 +19,30 @@ enum WeaponType
     case Sword;
     case Wand;
     case Whip;
+    /**
+     * @return Result<WeaponType|null>
+     */
+    public static function fromString(string $type): Result
+    {
+        return match ($type) {
+            'Axe'      => Result::success(null, self::Axe),
+            'Bow'      => Result::success(null, self::Bow),
+            'Dagger'   => Result::success(null, self::Dagger),
+            'Gauntlet' => Result::success(null, self::Gauntlet),
+            'Gun'      => Result::success(null, self::Gun),
+            'Mace'     => Result::success(null, self::Mace),
+            'Polearm'  => Result::success(null, self::Polearm),
+            'Staff'    => Result::success(null, self::Staff),
+            'Sword'    => Result::success(null, self::Sword),
+            'Wand'     => Result::success(null, self::Wand),
+            'Whip'     => Result::success(null, self::Whip),
+            default    => Result::error('Type not defined: ' . $type, null),
+        };
+    }
 
     public function toString(): string
     {
-        return match($this) {
+        return match ($this) {
             self::Axe      => 'Axe',
             self::Bow      => 'Bow',
             self::Dagger   => 'Dagger',
