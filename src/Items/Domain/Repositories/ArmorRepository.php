cc3a0a3353c3248bbba9a5cfc296730c7a3ea7d5
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-09-24 10:39:43 -0300

    chore: remove unused import

--- src/Items/Domain/Repositories/ArmorRepository.php
+++ src/Items/Domain/Repositories/ArmorRepository.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace AqHub\Items\Domain\Repositories;
 
-use AqHub\Shared\Domain\ValueObjects\{Result, StringIdentifier};
 use AqHub\Items\Domain\ValueObjects\ItemInfo;
+use AqHub\Shared\Domain\ValueObjects\Result;
 use AqHub\Items\Domain\Entities\Armor;
 
 interface ArmorRepository extends AqwItemRepository
