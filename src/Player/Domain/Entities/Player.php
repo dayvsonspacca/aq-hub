f50fc270ffa18f8736abc418d54f25cab701c19d
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-09-23 23:03:47 -0300

    feat: adds Name to Player

--- src/Player/Domain/Entities/Player.php
+++ src/Player/Domain/Entities/Player.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace AqHub\Player\Domain\Entities;
 
+use AqHub\Player\Domain\ValueObjects\{PlayerInventory, Name};
 use AqHub\Shared\Domain\ValueObjects\{Result, Identifier};
-use AqHub\Player\Domain\ValueObjects\PlayerInventory;
 use AqHub\Shared\Domain\Abstractions\Entity;
 
 class Player extends Entity
 {
     private function __construct(
         Identifier $id,
+        private readonly Name $name,
         private readonly int $level,
         private PlayerInventory $inventory
     ) {
@@ -21,6 +22,7 @@ class Player extends Entity
     /** @return Result<Player> */
     public static function create(
         Identifier $id,
+        Name $name,
         int $level,
         PlayerInventory $inventory
     ) {
@@ -28,7 +30,12 @@ class Player extends Entity
             return Result::error('The level of a player cant be negative.', null);
         }
 
-        return Result::success(null, new self($id, $level, $inventory));
+        return Result::success(null, new self($id, $name, $level, $inventory));
+    }
+
+    public function getName(): string
+    {
+        return $this->name->value;
     }
 
     public function getLevel(): int
---
+++ src/Player/Domain/ValueObjects/Name.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Player\Domain\ValueObjects;
+
+use AqHub\Shared\Domain\ValueObjects\Result;
+
+class Name
+{
+    private function __construct(public readonly string $value)
+    {
+    }
+
+    /**
+     * @return Result<Name|null>
+     */
+    public static function create(string $name): Result
+    {
+        $name = trim($name);
+
+        if (empty($name)) {
+            return Result::error('The name of a player cant be empty.', null);
+        }
+
+        return Result::success(null, new self($name));
+    }
+}
