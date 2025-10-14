1d68e308600055a379c49cfbab5d23b3b609a6b7
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-14 14:55:48 -0300

    tests(Route): covers Route construct

---
+++ tests/Unit/Shared/Infrastructure/Http/RouteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Tests\Unit\Shared\Infrastructure\Http;
+
+use AqHub\Shared\Infrastructure\Http\Route;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use Attribute;
+
+final class RouteTest extends TestCase
+{
+    #[Test]
+    public function should_be_created_with_correct_path_and_methods()
+    {
+        $path = '/armors/list';
+        $methods = ['GET'];
+
+        $route = new Route($path, $methods);
+
+        $this->assertSame($path, $route->path);
+        $this->assertSame($methods, $route->methods);
+    }
+}
