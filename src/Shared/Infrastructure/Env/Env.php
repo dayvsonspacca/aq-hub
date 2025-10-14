4d42208f8be32f546c59853d33e59e7ab3efc64c
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-14 12:41:03 -0300

    refactor: rename internal env data name + add option to force the recreation of the singleton instance, to make tests easy

--- src/Shared/Infrastructure/Env/Env.php
+++ src/Shared/Infrastructure/Env/Env.php
@@ -13,15 +13,15 @@ class Env
 
     private static ?self $instance = null;
 
-    private function __construct(array $env)
+    private function __construct(private readonly array $envRawData)
     {
-        $this->loadDatabaseConfig($env);
-        $this->loadAppMode($env);
+        $this->loadDatabaseConfig($envRawData);
+        $this->loadAppMode($envRawData);
     }
 
-    private function loadDatabaseConfig(array $env): void
+    private function loadDatabaseConfig(array $envRawData): void
     {
-        $result = DatabaseConfig::fromEnvironment($env);
+        $result = DatabaseConfig::fromEnvironment($envRawData);
 
         if ($result->isError()) {
             throw new RuntimeException($result->getMessage());
@@ -30,17 +30,17 @@ class Env
         $this->dbConfig = $result->getData();
     }
 
-    public static function instance(array $env): self
+    public static function instance(array $envRawData, bool $forceRecreation = false): self
     {
-        if (self::$instance === null) {
-            self::$instance = new self($env);
+        if (self::$instance === null || $forceRecreation) {
+            self::$instance = new self($envRawData);
         }
         return self::$instance;
     }
 
-    private function loadAppMode($env): void
+    private function loadAppMode($envRawData): void
     {
-        $mode = $env['APP_MODE'];
+        $mode = $envRawData['APP_MODE'];
 
         if ($mode === false) {
             throw new RuntimeException("Environment variable 'APP_MODE' is not set.");
