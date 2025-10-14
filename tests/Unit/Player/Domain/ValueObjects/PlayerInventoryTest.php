99703af6cf95cabdbca2c7b6f8b7ee6d6d93c961
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-14 14:30:42 -0300

    tests(PlayerInventory): covers error on delete item with ac tag

--- tests/Unit/Player/Domain/ValueObjects/PlayerInventoryTest.php
+++ tests/Unit/Player/Domain/ValueObjects/PlayerInventoryTest.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Tests\Unit\Player\Domain\ValueObjects;
 
 use AqHub\Items\Domain\Entities\{Armor, Weapon};
+use AqHub\Items\Domain\Enums\ItemRarity;
+use AqHub\Items\Domain\Services\ItemIdentifierGenerator;
+use AqHub\Items\Domain\ValueObjects\Description;
+use AqHub\Items\Domain\ValueObjects\ItemInfo;
+use AqHub\Items\Domain\ValueObjects\ItemTags;
+use AqHub\Items\Domain\ValueObjects\Name;
 use AqHub\Player\Domain\ValueObjects\PlayerInventory;
+use AqHub\Shared\Domain\Enums\TagType;
 use AqHub\Tests\Unit\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -63,7 +70,7 @@ final class PlayerInventoryTest extends TestCase
     }
 
     #[Test]
-    public function should_fail_when_add_sam_item_in_player_inventory()
+    public function should_fail_when_add_same_item_in_player_inventory()
     {
         $maxSpaces = 50;
         $weapon    = $this->createMock(Weapon::class);
@@ -111,5 +118,30 @@ final class PlayerInventoryTest extends TestCase
         foreach ($playerInventory as $index => $item) {
             $this->assertInstanceOf($index === 0 ? Weapon::class : Armor::class, $item);
         }
+    }
+
+    #[Test]
+    public function should_fail_when_try_delete_item_with_ac_tag()
+    {
+        $maxSpaces = 50;
+        $name        = 'ArchFiend DoomLord';
+        $description = "Not even the dark magic of the Shadowscythe is enough to satisfy this soul's lust for power.";
+        $tags        = new ItemTags([TagType::AdventureCoins]);
+        $itemInfo    = ItemInfo::create(Name::create($name)->unwrap(), Description::create($description)->unwrap(), $tags, ItemRarity::Epic)->unwrap();
+        $id          = ItemIdentifierGenerator::generate($itemInfo, Armor::class)->unwrap();
+
+        $armor = Armor::create(
+            $id,
+            $itemInfo,
+        )->unwrap();
+        $items     = [$armor];
+
+        $playerInventory = new PlayerInventory($items, $maxSpaces);
+
+        $result = $playerInventory->delete($armor);
+
+        $this->assertTrue($result->isError());
+        $this->assertTrue($playerInventory->has($armor));
+        $this->assertSame($result->getMessage(), 'You cant delete that item, AC tag present.');
     }
 }
