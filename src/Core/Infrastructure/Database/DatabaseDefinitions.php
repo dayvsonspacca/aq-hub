07eff0f05af655d1d15785ccf699de553fda492a
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-15 16:13:27 -0300

    feat: adds database definitions

---
+++ src/Core/Infrastructure/Database/DatabaseDefinitions.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Core\Infrastructure\Database;
+
+use AqHub\Core\Interfaces\DefinitionsInterface;
+use AqHub\Core\Env;
+
+use function DI\factory;
+use function DI\get;
+
+final class DatabaseDefinitions implements DefinitionsInterface
+{
+    public static function dependencies(): array
+    {
+        return [
+            PgsqlConnection::class => factory([PgsqlConnection::class, 'instance'])->parameter('env', get(Env::class))
+        ];
+    }
+}
---
+++ tests/Unit/Core/Infrastructure/Database/DatabaseDefinitionsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Tests\Unit\Core\Infrastructure\Database;
+
+use AqHub\Core\{ContainerFactory, CoreDefinitions, Env};
+use AqHub\Core\Infrastructure\Database\DatabaseDefinitions;
+use AqHub\Core\Infrastructure\Database\PgsqlConnection;
+use AqHub\Tests\TestCase;
+use DI\Container;
+use PHPUnit\Framework\Attributes\Test;
+
+final class DatabaseDefinitionsTest extends TestCase
+{
+    private Container $container;
+
+    protected function setUp(): void
+    {
+        $this->container = ContainerFactory::make(DatabaseDefinitions::dependencies());
+    }
+
+    #[Test]
+    public function should_return_dependencies()
+    {
+        $dependencies = DatabaseDefinitions::dependencies();
+
+        $this->assertCount(1, $dependencies);
+    }
+
+    #[Test]
+    public function should_have_database_definitions()
+    {
+        $this->assertTrue($this->container->has(PgsqlConnection::class));
+    }
+}
