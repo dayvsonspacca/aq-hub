801ea279ae815ca99d8c98bb237a723e2b113565
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-15 14:21:34 -0300

    feat: new base Application class

---
+++ src/Core/Application.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Core;
+
+use AqHub\Core\Interfaces\DefinitionsInterface;
+use DI\Container;
+use RuntimeException;
+use Throwable;
+
+/**
+ * The central coordinator (or Kernel) of the application.
+ *
+ * This class is responsible for bootstrapping the Dependency Injection (DI)
+ * Container and providing access to the application's core services.
+ */
+final class Application
+{
+    /**
+     * @param Container $container The configured Dependency Injection container instance.
+     */
+    private function __construct(
+        private readonly Container $container
+    ) {}
+
+    /**
+     * Factory method responsible for creating and configuring the Application instance.
+     *
+     * It uses a list of Definition classes to load all dependencies and parameters
+     * into the DI container before the Application is ready to be used.
+     *
+     * @param string[] $definitions An array of classes that implement DefinitionsInterface, anything that don't implement it is ignored.
+     * @return self|null The fully configured Application instance, or null if bootstrapping fails.
+     */
+    public static function build(string $name, array $definitions): ?self
+    {
+        try {
+            $definitions = array_filter($definitions, fn(string $definition) => (new $definition) instanceof DefinitionsInterface);
+            $config = array_map(fn(string $definition) => $definition::dependencies(), $definitions);
+
+            return new self(ContainerFactory::make(array_merge(...$config)));
+        } catch (Throwable $e) {
+            throw new RuntimeException('Fatal error bootstraping ' . $name . ' cause: ' . $e->getMessage(), previous: $e);
+        }
+    }
+}
---
+++ tests/Unit/Core/ApplicationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqHub\Tests\Unit\Core;
+
+use AqHub\Core\Application;
+use AqHub\Core\CoreDefinations;
+use AqHub\Core\Interfaces\DefinitionsInterface;
+use AqHub\Tests\TestCase;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\Test;
+use RuntimeException;
+
+final class ApplicationTest extends TestCase
+{
+    #[Test]
+    public function should_bootstrap_application()
+    {
+        $app = Application::build('api', [CoreDefinations::class]);
+
+        $this->assertInstanceOf(Application::class, $app);
+    }
+
+    #[Test]
+    public function should_fail_bootstraping_application()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Fatal error bootstraping api cause: Invalid definition');
+        $this->expectExceptionCode(0);
+
+        Application::build('api', [
+            CoreDefinations::class,
+            (new class implements DefinitionsInterface {
+                public static function dependencies(): array
+                {
+                    throw new InvalidArgumentException('Invalid definition');
+                }
+            })::class
+        ]);
+    }
+}
