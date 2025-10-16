634a8f888a83aa7a205597b2bac5bc3bad1a7e5f
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-16 18:02:22 -0300

    docker: insert basic entries to test pgsql classes

---
+++ docker/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO armors (name, hash, description, rarity) VALUES
+('ArchFiend DoomLord', 'f8aef10218bdf49e1c7215a17f8c13da', 'Not even the dark magic of the Shadowscythe is enough to satisfy this soul''s lust for power. This item does 35% more damage to any monster, and gives 25% more class points, XP, gold, and rep when equipped.', 'Epic');
+
+INSERT INTO armor_tags (armor_id, tag) VALUES
+(1, 'Adventure Coins');
