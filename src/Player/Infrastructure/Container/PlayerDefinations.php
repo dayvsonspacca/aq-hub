693ecf0c6096b29fd147c701ed6080100201976f
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-16 17:17:16 -0300

    refactor: fix player definitions

--- src/Player/Infrastructure/Container/PlayerDefinations.php
+++ src/Player/Infrastructure/Container/PlayerDefinations.php
@@ -4,63 +4,12 @@ declare(strict_types=1);
 
 namespace AqHub\Player\Infrastructure\Container;
 
-use AqHub\Player\Application\UseCases\{AddPlayer, FindAllPlayers, MarkAsMined, PlayerUseCases};
-use AqHub\Player\Domain\Repositories\PlayerRepository;
-use AqHub\Player\Infrastructure\Console\MinePlayersNameCommand;
-use AqHub\Player\Infrastructure\Http\Controllers\PlayerController;
-use AqHub\Player\Infrastructure\Repositories\Sql\SqlPlayerRepository;
-use AqHub\Shared\Infrastructure\Container\Definations;
-use AqHub\Shared\Infrastructure\Database\Connection;
+use AqHub\Core\Interfaces\DefinitionsInterface;
 
-use function DI\{autowire, get};
-
-class PlayerDefinations implements Definations
+class PlayerDefinations implements DefinitionsInterface
 {
-    public static function getDefinitions(): array
+    public static function dependencies(): array
     {
-        return array_merge(
-            self::repositories(),
-            self::useCases(),
-            self::commands(),
-            self::controllers()
-        );
-    }
-
-    public static function repositories(): array
-    {
-        return [
-            SqlPlayerRepository::class => autowire()->constructor(get(Connection::class)),
-            PlayerRepository::class => autowire(SqlPlayerRepository::class)
-        ];
-    }
-
-    public static function commands(): array
-    {
-        return [
-            MinePlayersNameCommand::class => autowire()
-        ];
-    }
-
-    public static function useCases(): array
-    {
-        return [
-            AddPlayer::class => autowire()->constructor(
-                get(PlayerRepository::class),
-                get('Cache.Players')
-            ),
-            FindAllPlayers::class => autowire()->constructor(
-                get(PlayerRepository::class),
-                get('Cache.Players')
-            ),
-            MarkAsMined::class => autowire(),
-            PlayerUseCases::class => autowire(),
-        ];
-    }
-
-    public static function controllers(): array
-    {
-        return [
-            PlayerController::class => autowire()
-        ];
+        return [];
     }
 }
