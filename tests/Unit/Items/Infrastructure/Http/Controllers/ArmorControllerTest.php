8b3734c73706a33b91221f7787714047f915bd71
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-10-16 21:42:56 -0300

    tests(ArmorController): use container to test controller

--- tests/Unit/Items/Infrastructure/Http/Controllers/ArmorControllerTest.php
+++ tests/Unit/Items/Infrastructure/Http/Controllers/ArmorControllerTest.php
@@ -4,28 +4,43 @@ declare(strict_types=1);
 
 namespace AqHub\Tests\Unit\Items\Infrastructure\Http\Controllers;
 
+use AqHub\Core\ContainerFactory;
+use AqHub\Core\CoreDefinitions;
+use AqHub\Core\Infrastructure\Database\DatabaseDefinitions;
+use AqHub\Core\Infrastructure\Http\HttpDefinitions;
 use AqHub\Items\Infrastructure\Http\Controllers\ArmorController;
 use AqHub\Tests\TestCase;
 use AqHub\Tests\Traits\DoRequests;
+use DI\Container;
 use PHPUnit\Framework\Attributes\Test;
 
 class ArmorControllerTest extends TestCase
 {
     use DoRequests;
 
+    private Container $container;
+    private ArmorController $controller;
+
+    protected function setUp(): void
+    {
+        $this->container = ContainerFactory::make(array_merge(
+            CoreDefinitions::dependencies(),
+            HttpDefinitions::dependencies(),
+            DatabaseDefinitions::dependencies()
+        ));
+
+        $this->controller = $this->container->get(ArmorController::class);
+    }
+
     #[Test]
     public function should_create_armor_controller()
     {
-        $controller = new ArmorController();
-
-        $this->assertInstanceOf(ArmorController::class, $controller);
+        $this->assertInstanceOf(ArmorController::class, $this->controller);
     }
 
     #[Test]
     public function should_list_armors()
     {
-        $controller = new ArmorController();
-
-        $this->assertNull($controller->list($this->makeRequest()));
+        $this->assertNull($this->controller->list($this->makeRequest()));
     }
 }
