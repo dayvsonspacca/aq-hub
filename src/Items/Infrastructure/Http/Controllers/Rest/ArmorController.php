19bfcc3e68867a54fcb2ddbed3535f2c606fd266
dayvsonspacca <64930283+dayvsonspacca@users.noreply.github.com>
2025-11-21 14:05:56 +0000

    ci: apply code formatting via composer fmt

--- src/Items/Infrastructure/Http/Controllers/Rest/ArmorController.php
+++ src/Items/Infrastructure/Http/Controllers/Rest/ArmorController.php
@@ -8,19 +8,18 @@ use AqHub\Core\Infrastructure\Http\Interfaces\RestController;
 use AqHub\Core\Infrastructure\Http\Route;
 use AqHub\Items\Application\Armors\Queries\FindAll;
 use AqHub\Items\Infrastructure\Http\Forms\ListAllArmorsForm;
-use AqHub\Items\Infrastructure\Http\OpenAPI\QueryParameters\NameParameter;
-use AqHub\Items\Infrastructure\Http\OpenAPI\QueryParameters\RaritiesParameter;
-use AqHub\Items\Infrastructure\Http\OpenAPI\QueryParameters\TagsParamenter;
+use AqHub\Items\Infrastructure\Http\OpenAPI\QueryParameters\{NameParameter, RaritiesParameter, TagsParamenter};
 use AqHub\Items\Infrastructure\Http\OpenAPI\Response\ListArmorsResponse;
 use AqHub\Shared\Domain\Helpers\ArrayPresenter;
-use AqHub\Shared\Infrastructure\Http\OpenAPI\QueryParameters\PageParameter;
-use AqHub\Shared\Infrastructure\Http\OpenAPI\QueryParameters\PageSizeParameter;
+use AqHub\Shared\Infrastructure\Http\OpenAPI\QueryParameters\{PageParameter, PageSizeParameter};
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 
 class ArmorController implements RestController
 {
-    public function __construct(private readonly FindAll $findAll) {}
+    public function __construct(private readonly FindAll $findAll)
+    {
+    }
 
     #[OA\Get(
         path: '/armors/list',
--- src/Items/Infrastructure/Http/OpenAPI/Schema/ArmorFilterSchema.php
+++ src/Items/Infrastructure/Http/OpenAPI/Schema/ArmorFilterSchema.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace AqHub\Items\Infrastructure\Http\OpenAPI\Schema;
 
-use AqHub\Items\Infrastructure\Http\OpenAPI\Properties\Filters\NameProperty;
-use AqHub\Items\Infrastructure\Http\OpenAPI\Properties\Filters\RaritiesProperty;
-use AqHub\Items\Infrastructure\Http\OpenAPI\Properties\Filters\TagsProperty;
-use AqHub\Shared\Infrastructure\Http\OpenAPI\Properties\Filters\PageProperty;
-use AqHub\Shared\Infrastructure\Http\OpenAPI\Properties\Filters\PageSizeProperty;
+use AqHub\Items\Infrastructure\Http\OpenAPI\Properties\Filters\{NameProperty, RaritiesProperty, TagsProperty};
+use AqHub\Shared\Infrastructure\Http\OpenAPI\Properties\Filters\{PageProperty, PageSizeProperty};
 use OpenApi\Attributes as OA;
 
 #[OA\Schema(
