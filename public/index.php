5ed9e7dbda2a2e02ee1dc2c7d71f2556c99588e8
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-12-19 14:03:42 -0300

    feat: adds basic api auth

--- public/index.php
+++ public/index.php
@@ -6,11 +6,12 @@ use AqHub\Core\Infrastructure\Http\{HttpHandler, HttpDefinitions};
 use AqHub\Core\{Application, CoreDefinitions};
 use AqHub\Core\Infrastructure\Database\DatabaseDefinitions;
 use AqHub\Items\Infrastructure\Container\ItemsDefinitions;
+use AqHub\Shared\Infrastructure\Container\SharedDefinitions;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app     = Application::build('api', [CoreDefinitions::class, HttpDefinitions::class, DatabaseDefinitions::class, ItemsDefinitions::class]);
+$app     = Application::build('api', [CoreDefinitions::class, HttpDefinitions::class, DatabaseDefinitions::class, ItemsDefinitions::class, SharedDefinitions::class]);
 $handler = $app->get(HttpHandler::class);
 
 $request = Request::createFromGlobals();
--- src/Shared/Infrastructure/Container/SharedDefinitions.php
+++ src/Shared/Infrastructure/Container/SharedDefinitions.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace AqHub\Shared\Infrastructure\Container;
 
+use AqHub\Core\Infrastructure\Database\PgsqlConnection;
 use AqHub\Core\Interfaces\DefinitionsInterface;
 use AqHub\Shared\Infrastructure\Http\Controllers\Rest\ApiAuthController;
 use AqHub\Shared\Infrastructure\Http\Middlewares\JwtAuthMiddleware;
 use AqHub\Shared\Infrastructure\Http\Services\JwtAuthService;
+use AqHub\Shared\Infrastructure\Repositories\Pgsql\PgsqlUsersApiRepository;
 
 use function DI\add;
 use function DI\autowire;
@@ -23,7 +25,9 @@ class SharedDefinitions implements DefinitionsInterface
                 get(ApiAuthController::class)
             ]),
             JwtAuthService::class => autowire(),
-            JwtAuthMiddleware::class => autowire()
+            JwtAuthMiddleware::class => autowire(),
+
+            PgsqlUsersApiRepository::class => autowire()->constructor(get(PgsqlConnection::class), get('QueryBuilder.Pgsql'))
         ];
     }
 }
